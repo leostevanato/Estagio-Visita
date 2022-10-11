9e41abdd5292615ebfb29bba491d3a77044a63a7
leostevanato <47180899+leostevanato@users.noreply.github.com>
2022-10-11 09:02:58 -0300

    Criar template página confirma inscrição Moodle
    - IMPORTANTE: Lembrar de adaptar a página de confirmação do conteúdo para funcionar também com a estrutura da página principal de cursos do Moodle.
    - No Estágio Visita a página de confirmação de inscrição do Moodle não é usada, então o template deve ser adaptado para ser usado na página principal e quando o curso começar ele será substituído pela página de conteúdo.
    - Nessa primeira versão, por causa do prazo apertado, a confirmação de inscrição será apenas uma imagem.

---
+++ js/confirma-inscricao.js
@@ -0,0 +1,65 @@
+// Carrega a div de um arquivo externo e adiciona a div ao HTML alvo.
+if (document.querySelector('body').classList.contains('path-mod-page')) {
+	var url_mdl_arquivos = '';
+
+	if (codigo_pasta_arquivos) {
+		var url_mdl_arquivos = 'https://www.educacaoadistancia.camara.leg.br/ead_cfd/pluginfile.php/' + codigo_pasta_arquivos + '/mod_folder/content/0/';
+	}
+
+	let response_promise = fetch(url_mdl_arquivos + 'html-confirma-inscricao.html');
+
+	let curso_promise = fetch(url_mdl_arquivos + 'curso-configuracoes.json', {
+		headers: {
+			'Accept': 'application/json'
+		}
+	});
+
+	var elemento_conteudo_seletor = '#curso-confirma-inscricao';
+	var elemento_alvo_seletor = '.path-mod-page #region-main > [role=main] > .box';
+	var data_periodo_seletor = '#page-header ul.breadcrumb > li.breadcrumb-item:last-child > a';
+	var botao_cta_seletor = elemento_conteudo_seletor + ' > .conteudo > .botao-cta';
+
+	document.addEventListener("DOMContentLoaded", function () {
+		response_promise.then(response => {
+			return response.text();
+		}).then(responseHTMLtext => {
+			// This is the HTML from our response as a text string
+			var parser = new DOMParser();
+			var docHTML = parser.parseFromString(responseHTMLtext, 'text/html');
+			
+			var elemento_conteudo = docHTML.querySelector(elemento_conteudo_seletor);
+			var elemento_alvo = document.querySelector(elemento_alvo_seletor);
+			
+			elemento_alvo.innerHTML = elemento_conteudo.outerHTML;
+
+			let data_periodo = document.querySelector(data_periodo_seletor).textContent.trim();
+
+			let regex_data_periodo = /([0-9]{2}[\-/ \.][0-9]{2})\s*-\s*(.+)/;
+			var data_periodo_result = data_periodo.match(regex_data_periodo);
+
+			document.querySelector(botao_cta_seletor).addEventListener("click", function (event) {
+				if (this.getAttribute("data-link") != null) {
+					window.open(this.getAttribute("data-link"));
+					return false;
+				}
+			});
+			
+			curso_promise.then(response => {
+				return response.json();
+			}).then(cursoJSON => {
+
+			}).catch(error => {
+				console.log("ERRO: " + error);
+			});
+		}).catch(function (erro) {
+			console.warn('ERRO: ', erro);
+		});
+
+		document.querySelector(botao_cta_seletor).addEventListener("click", function (event) {
+			if (this.getAttribute("data-link") != null) {
+				window.open(this.getAttribute("data-link"));
+				return false;
+			}
+		});
+	}, false);
+}
